d7c92e143b004b2c882451710c8bd950ceb70b6a
Sayani Deb <87908951+SayaniDeb10@users.noreply.github.com>
2026-03-05 11:44:05 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3);//we are using node.js , not browser
+
+let name = "Sayani"
+let age = 25;
+let isLoggedIn = false
+
+//object
+
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
