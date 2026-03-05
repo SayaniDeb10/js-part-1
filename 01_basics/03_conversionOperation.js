d9b6c93c26d0621dc732238d3c38a9e8e84a8bc3
Sayani Deb <87908951+SayaniDeb10@users.noreply.github.com>
2026-03-05 12:33:53 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+let score ="33abc"
+let score1 = null
+let score2 = undefined
+let score3 = true
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+let valueInNumber1 = Number(score1);
+let valueInNumber2 = Number(score2);
+let valueInNumber3 = Number(score3);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);//NaN
+// console.log(valueInNumber1);//0
+// console.log(valueInNumber2);//NaN
+// console.log(valueInNumber3);//1
+
+let isLoggedIn = 1
+let isLoggedIn1 = ""
+let isLoggedIn2 = "Sayani"
+let isLoggedIn3 = null
+let isLoggedIn4 = undefined
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
+let booleanIsLoggedIn3 = Boolean(isLoggedIn3)
+let booleanIsLoggedIn4 = Boolean(isLoggedIn4)
+
+// console.log(booleanIsLoggedIn);//true
+// console.log(booleanIsLoggedIn1);//false
+// console.log(booleanIsLoggedIn2);//true
+// console.log(booleanIsLoggedIn3);//false
+// console.log(booleanIsLoggedIn4);//false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
